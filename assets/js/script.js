9db29e9dc578d8547784eda1d943e4795e2a819e
Amadewe <dewevre.amandine@gmail.com>
2019-01-22 12:16:52 +0100

    Ajout de l'exercice 2 partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+alert('Merci de votre participation');
+
+
+
+
+function displayFocus(){
+  var elem=document.getElementById('lastname');
+  lastname.setAttribute('value','Merci de votre participation');
+}
