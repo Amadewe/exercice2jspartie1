bbf08aa40ab12188784c1aa6f55e159b75706840
Amadewe <dewevre.amandine@gmail.com>
2019-01-22 14:13:27 +0100

    Fin de l'exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-alert('Merci de votre participation');
+var elem = document.getElementById("lastname");
 
+// Attention : message(); ou message(param); ne fonctionneront pas ici,
+// il doit s'agir d'une référence au nom d'une fonction et non d'un appel de cette fonction
 
+elem.onblur = message;
 
-
-function displayFocus(){
-  var elem=document.getElementById('lastname');
-  lastname.setAttribute('value','Merci de votre participation');
+function message() {
+  alert("Merci de votre participation");
 }
+
+// La propriété onblur renvoie le gestionnaire de l'évènement blur (perte de focus) pour l'élément courant.
